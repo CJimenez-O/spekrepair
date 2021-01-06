77a10979476dd0fdf43aef60c61ccb5e6ad2acb8
legazy33 <chris.jimenez50@yahoo.com>
2021-01-05 21:08:29 -0800

    adjusted landing page

--- js/index.js
+++ js/index.js
@@ -35,6 +35,8 @@ window.addEventListener("scroll", () => {
 
 gsap.from(".hamburger", { opacity: 0, duration: 1, delay: 1, x: 20 });
 gsap.from(".hero-img", { opacity: 0, duration: 1, delay: 1.5, x: -200 });
-gsap.from(".hero-content h2", { opacity: 0, duration: 1, delay: 2, y: -50 });
+
+gsap.from(".hero-content h3", { opacity: 0, duration: 1, delay: 2, y: -50 });
+gsap.from(".hero-content h2", { opacity: 0, duration: 1, delay: 3, y: -50 });
 gsap.from(".hero-content h1", { opacity: 0, duration: 1, delay: 2.5, y: -45 });
 gsap.from(".hero-content a", { opacity: 0, duration: 1, delay: 3.5, y: 50 });
